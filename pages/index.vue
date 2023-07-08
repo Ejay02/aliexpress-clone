d10ede1e0b7fdb787d2e464fa7b098530e889cad
Ejay02 <e.jae02@gmail.com>
2023-07-08 19:48:06 +0100

    scaffold project, navbar WIP

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main-layout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      IndexPag
+    </div>
+  </main-layout>
+</template>
+
+<script setup>
+import mainLayout from "../layouts/mainLayout.vue";
+</script>
